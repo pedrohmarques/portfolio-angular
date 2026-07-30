d15578731887b493af2c6dbfd3fe69e11ac504bf
Pedro Almeida <pdhmarques@gmail.com>
2026-07-29 22:04:47 -0300

    REsume

--- src/app/views/components/resume/resume.ts
+++ src/app/views/components/resume/resume.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-resume',
-  imports: [],
+  imports: [
+    TranslocoPipe,
+    MatIconModule
+  ],
   templateUrl: './resume.html',
   styleUrl: './resume.scss',
 })
 export class Resume {
 
+  educations = [
+    {
+      year: "2026",
+      education: "Bacharelado em Engenharia de Software",
+      educationEN: "Software Engineer Bachelor Degree",
+      location: "Universidade PUC Minas",
+      locationEN: "PUC Minas University"
+    },
+    {
+      year: "2016",
+      education: "Curso de Informática",
+      educationEN: "IT Course",
+      location: "UTRAMIG",
+      locationEN: "UTRAMIG"
+    },
+    {
+      year: "2016",
+      education: "Ensino Médio Completo",
+      educationEN: "High School Education",
+      location: "",
+      locationEN: ""
+    }
+  ]
 }
